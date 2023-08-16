9bd00dec463065c9d37c338e2558d2361207668b
EricNguyenFly <huynguyen.flyteam@gmail.com>
2023-08-16 16:32:11 +0700

    add test sepolia

--- scripts/verify.js
+++ scripts/verify.js
@@ -4,11 +4,17 @@ const { run } = require("hardhat");
 async function main() {
     const jobs = [
         run("verify:verify", {
-            address: "0x451a32Fe376a699Ea25b6Cafc00E446ECC8452A9",
+            address: "0xc82f14458f68f076A4f2E756dB24B56A3C670bB4",
         }),
-        // run("verify:verify", {
-        //     address: contracts.monkey1155Verify,
-        // }),
+        run("verify:verify", {
+            address: "0x943931387b8659A74752c8D7B890870899b4Fdaf",
+            constructorArguments: ["0xfB2b22611F716996281FB1CBA70411848DD2d864"]
+        }),
+        run("verify:verify", {
+            contract: "contracts/test/ERC20.sol:ERC20",
+            address: "0x764EcF27DF3df771D1c79f48A05aB18d2b6BBa10",
+            constructorArguments: ["1000000000000000000000000000"]
+        }),
     ];
 
     await Promise.all(jobs.map((job) => job.catch(console.log)));
